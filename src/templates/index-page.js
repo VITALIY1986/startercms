9a87240f492d00fbca35a30f1d6863fdc7709a8e
VITALIY1986 <staslego1986@gmail.com>
2020-02-03 00:48:06 +0200

    made

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -87,12 +87,13 @@ export const IndexPageTemplate = ({
                       {heading}
                     </h3>
                     <p>{description}</p>
-                    <p>{paragraphs}</p>
+           
                   </div>
                 </div>
                 <Features gridItems={intro.blurbs} />
                 <div className="columns">
                   <div className="column is-12 has-text-centered">
+                    <h1>{paragraphs}</h1>
                     <Link className="btn" to="/products">
                       See all products
                     </Link>
